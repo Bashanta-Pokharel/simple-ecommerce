3b1ffbec36776f2d5e5f83dfe40f784c86463a3b
Bashanata62 <pbasanta340@gmail.com>
2026-06-21 21:28:54 +0545

    Implement add to cart functionality

---
+++ addtocart.php
@@ -0,0 +1,27 @@
+<?php
+include 'db.php';
+
+$id = (int) ($_GET['id'] ?? 0);
+$stmt = mysqli_prepare($conn, 'SELECT * FROM products WHERE id = ?');
+mysqli_stmt_bind_param($stmt, 'i', $id);
+mysqli_stmt_execute($stmt);
+$product = mysqli_fetch_assoc(mysqli_stmt_get_result($stmt));
+
+if (!$product) {
+    header('Location: products.php');
+    exit;
+}
+
+$name = product_name($product);
+$price = $product['price'];
+$image = $product['image'];
+$stmt = mysqli_prepare($conn, 'INSERT INTO cart(product_id, name, price, image) VALUES(?, ?, ?, ?)');
+mysqli_stmt_bind_param($stmt, 'isds', $id, $name, $price, $image);
+mysqli_stmt_execute($stmt);
+
+if (isset($_GET['buy'])) {
+    header('Location: checkout.php');
+} else {
+    header('Location: cart.php');
+}
+exit;
